4287f90d6bb20c336196d137112fffdb0899833c
yuki202304 <131171459+yuki202304@users.noreply.github.com>
2023-09-16 14:34:51 +0900

    file update

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+  $('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+    
+  var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
+    
+  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+    $(this).removeClass('close');//クラス名を除去    
+  }else{//それ以外は
+    $('.close').removeClass('close'); //クラス名closeを全て除去した後
+    $(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+    $(findElm).slideDown(500);//アコーディオンを開く
+  }
+});
+
+
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+  if(hashIDName){
+    //タブ設定
+    $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+      var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
+      if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+        var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+        $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+        $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+        //表示させるエリア設定
+        $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+        $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加 
+      }
+    });
+  }
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+  var idName = $(this).attr('href'); //タブ内のリンク名を取得  
+  GethashID (idName);//設定したタブの読み込みと
+  return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+  var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+  GethashID (hashName);//設定したタブの読み込み
+});
