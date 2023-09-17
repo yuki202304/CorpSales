ae293ef222f7a994a6d6713cf67ad3707d669e27
yuki202304 <131171459+yuki202304@users.noreply.github.com>
2023-09-17 20:44:25 +0900

    Add files via upload

---
+++ footer-chart.php
@@ -0,0 +1,7 @@
+</main>
+</script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script>
+</body>
+</html>
