6bc5b168049074b59c9a101d92c6247490c16924
yuki202304 <131171459+yuki202304@users.noreply.github.com>
2023-09-17 21:58:10 +0900

    Update header-auto.php

--- header-auto.php
+++ header-auto.php
@@ -13,7 +13,7 @@
     <!-- jQuery 郵便番号自動生成 -->
     <script src="//yubinbango.github.io/yubinbango/yubinbango.js" charset="UTF-8"></script>
     <!-- jQuery autocompleteを使った自動補完 -->
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
 <script src="//code.jquery.com/jquery-3.5.1.js"></script>
 <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
