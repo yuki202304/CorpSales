f4c3d9c11e747fa4e26c57cc5bb4a8538f84352a
yuki202304 <131171459+yuki202304@users.noreply.github.com>
2023-09-18 12:10:34 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
         if(isset($_SESSION['login'])) { ?>
                 <nav>
                         <ul class="header-menu">
-                                <li><h1><a href="index.php"><img class="logo opacity" src="img/icon.svg" alt="ロゴ"></a>CorpSales<span>/コープセールス</span></h1></li>
+                                <li><h1><a href="index.php"><img class="logo opacity" src="img/icon3.svg" alt="ロゴ"></a>CorpSales</h1></li>
                                 <li><a id="list" href="history.php">企業一覧</a></li>
                                 <li><a id="list" href="company.php">企業登録</a></li>
                                 <li><a id="list" href="customer.php">担当者登録</a></li>
@@ -18,7 +18,7 @@
         } else { ?>
                 <nav>
                         <ul class="header-menu">
-                                <li><h1><a href="index.php"><img class="logo opacity" src="img/icon.svg" alt="ロゴ"></a>CorpSales<span>/コープセールス</span></h1></li>
+                                <li><h1><a href="index.php"><img class="logo opacity" src="img/icon3.svg" alt="ロゴ"></a>CorpSales</h1></li>
                                 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　
                                 <li><a id="list" href="login-input.php">ログイン</a></li>
                         </ul>
